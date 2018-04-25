92bf7ad1e66795f6d7405a74199dd9c61d69d1ab
miromax21 <miromax21@gmail.com>
2018-04-25 16:13:43 +0400

    * routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { UserComponent } from '../app/user/profile/user.component';
 import { FriendsComponent } from '../app/user/friends/friends.component';
 import { AuthGuard, RoutingConsts } from './shared';
 const routes: Routes = [
+    { path: '', redirectTo: RoutingConsts.user, pathMatch: 'full' },
     { path: RoutingConsts.friends, component: FriendsComponent, canActivate: [AuthGuard] },
     { path: RoutingConsts.user, component: UserComponent, canActivate: [AuthGuard] },
     { path: '**', component: PageNotFoundComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes, {
+        paramsInheritanceStrategy: 'always',
+    })],
     exports: [RouterModule]
 })
 export class AppRoutingModule {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { RoutingConsts } from './shared/enums/routing-consts.enum';
+
 @Component({
   selector: 'app-root',
   template: `
+  
   <div class="layout">
-
     <div class="container">
       <div class="tabs">
-        <a routerLink="/user" class="tab" routerLinkActive="active-link">Профиль</a>
-        <a routerLink="/friends" class="tab"  routerLinkActive="active-link">Друзья прользователя</a>
+        <a [routerLink]="[routingConsts.user]" class="tab" routerLinkActive="active-link">Профиль</a>
+        <a [routerLink]="[routingConsts.friends]" class="tab"  routerLinkActive="active-link">Друзья прользователя</a>
       </div>
       <router-outlet></router-outlet>
     </div>
@@ -15,4 +17,5 @@ import { Component } from '@angular/core';
              `
 })
 export class AppComponent {
+  routingConsts: any = RoutingConsts;
 }
