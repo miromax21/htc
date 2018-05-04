de994a506025516ec3130df380d3d96ce044880b
miromax21 <miromax21@gmail.com>
2018-05-04 15:45:53 +0400

    *delet trash

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -4,8 +4,5 @@ export { LocalStorageService, LocalStorageEnum } from './services/local-storage.
 export { ServiceLocator } from './service-locator';
 export { RoutingConsts } from './enums/routing-consts.enum';
 export { Utils } from './utils';
-export { IDropdown } from './components/dropdown/dropdown.component';
-export { DropdownOption } from './components/dropdown/dropdown-option.model';
-export { IExpandable } from './components/expandable/expandable.component';
 export { AuthGuard } from './auth.guard';
 export { FocusDirective } from './components/FocusDirective';
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,30 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
-// import { StatisticService } from './services/statistic.service';
-// import { CampaignService } from './services/campaign.service';
-// import { CountryService } from './services/country.service';
 import { AuthorizationService } from './services/authorization.service';
 import { LocalStorageService } from './services/local-storage.service';
 import { AuthGuard } from './auth.guard';
-// import { AdvertService } from './services/advert.service';
-// import { HttpCacheService } from './services/http-cache.service';
-// import { AdvertGuard } from './advert.guard';
-// import { AuthInterceptor } from './interceptors/auth.interceptor';
-// import { CacheInterceptor } from './interceptors/cache.interceptor';
-// import { UrlInterceptor } from './interceptors/url.interceptor';
-
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderUnauthorizedComponent } from './header/header-unauthorized/header-unauthorized.component';
-import { HeaderAuthorizedComponent } from './header/header-authorized/header-authorized.component';
-import { ExpandableComponent } from './components/expandable/expandable.component';
 import { FocusDirective } from './components/FocusDirective';
-import { DropdownComponent } from './components/dropdown/dropdown.component';
-import { PopupComponent } from './components/popup/popup.component';
-// import { DatePickerComponent } from './components/datePicker/date-picker.component';
-// import { MyNumberPipe } from './pipes/my-number.pipe';
 
 
 @NgModule({
@@ -36,8 +16,8 @@ import { PopupComponent } from './components/popup/popup.component';
         RouterModule,
         FormsModule
     ],
-    declarations: [HeaderComponent, FooterComponent, HeaderUnauthorizedComponent, HeaderAuthorizedComponent, ExpandableComponent, DropdownComponent, PopupComponent],
-    exports: [HeaderComponent, FooterComponent, ExpandableComponent, DropdownComponent, PopupComponent]
+    declarations: [],
+    exports: []
 })
 export class SharedModule {
 
@@ -48,32 +28,6 @@ export class SharedModule {
                 LocalStorageService,
                 AuthorizationService,
                 AuthGuard
-                //     AccountService,
-                //     StatisticService,
-                //     CampaignService,
-                //     CountryService,
-                //
-
-                //    LocalStorageService
-                //     AdvertService,
-                //     HttpCacheService,
-                //     AuthGuard,
-                //     AdvertGuard,
-                //     {
-                //         provide: HTTP_INTERCEPTORS,
-                //         useClass: UrlInterceptor,
-                //         multi: true
-                //     },
-                //     {
-                //         provide: HTTP_INTERCEPTORS,
-                //         useClass: CacheInterceptor,
-                //         multi: true
-                //     },
-                //  {
-                //      provide: HTTP_INTERCEPTORS,
-                ///      useClass: AuthInterceptor,
-                //      multi: true
-                //  }
             ]
         };
     }
